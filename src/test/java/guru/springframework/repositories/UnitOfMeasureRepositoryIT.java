8ff245e8a11aae3df8fd74966cd00b3e660a4720
dima-lvov <Dimon>
2017-11-07 23:20:36 +0200

    UPDATE: Add integration test for UnitOfMeasureRepository

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,31 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Dimon on 07.11.2017
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Test
+	public void findByDescription() throws Exception {
+		final Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertThat(uomOptional.get().getDescription(), equalTo("Teaspoon"));
+	}
+}
