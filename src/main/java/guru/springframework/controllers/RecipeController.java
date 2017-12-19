9252630f2034ceac94861aee5ef81a877f6849bb
dima-lvov <Dimon>
2017-12-20 00:06:26 +0200

    UPDATE: Save operation

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -25,6 +28,21 @@ public class RecipeController {
 	public String getById(final Model model, final @PathVariable("id") Long id) {
 		model.addAttribute("recipe", recipeService.findById(id));
 		return "/recipe/show";
+	}
+
+	@RequestMapping("/new")
+	public String newRecipe(Model model){
+		model.addAttribute("recipe", new RecipeCommand());
+
+		return "recipe/recipeform";
+	}
+
+	@PostMapping
+	@RequestMapping
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 }
 
